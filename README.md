1a1900afc5c7fd4cea5bc873a0f9f8609d88c5ba
Atuma David <atumadavid35@gmail.com>
2025-05-17 19:14:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Nestjs eCommerce API
+
+A RESTful API for an eCommerce platform built with NestJS, PostgreSQL, and TypeORM.
+Dependencies
+
+- typeorm: ORM for database operations (`npm install typeorm --save`)
+- pg: PostgreSQL driver for Node.js (`npm install pg --save`)
+- reflect-metadata: Metadata reflection for TypeScript (`npm install reflect-metadata --save`)
+- node typings: (`npm install @types/node --save-dev`)
+- nestjs typeorm integration (`npm install --save @nestjs/typeorm`)
+- dotenv (`npm i dotenv`)
+
+## Modules
+
+- users
+- products
+- orders
+- shipping info
+- category
---
+++ db/data-source.ts
@@ -0,0 +1,24 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config } from 'dotenv';
+
+// Load environment variables from .env file
+config();
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  // host: process.env.DB_HOST,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: false,
+  migrations: [],
+  // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  entities: ['dist/**/*.entity{.ts,.js}'],
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;
