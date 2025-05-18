09bbf31af67f3c905378b622318fea96eabb7400
Atuma David <atumadavid35@gmail.com>
2025-05-18 15:59:18 +0100

    Add authentication with JWT and password hashing
    Implements user authentication functionality with the following features:
    
    - Adds JWT-based authentication for secure user sessions
    - Implements password hashing using bcrypt for secure storage
    - Adds signin endpoint with token generation
    - Makes email unique and hides password from queries
    - Adds timestamp columns for user creation/updates
    - Updates user signup to check for existing users
    
    Dependencies added:
    - jsonwebtoken for JWT handling
    - bcrypt for password hashing

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,6 +12,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserSignUpDto } from './dto/user-signup.dto';
 import { UserEntity } from './entities/user.entity';
+import { UserSignInDto } from './dto/user-signin.dto';
 
 @Controller('users')
 export class UsersController {
@@ -22,6 +23,17 @@ export class UsersController {
     @Body() signupUser: UserSignUpDto,
   ): Promise<{ UserData: UserEntity }> {
     return { UserData: await this.usersService.signup(signupUser) };
+  }
+
+  @Post('signin')
+  async signin(@Body() userSignin: UserSignInDto): Promise<{
+    accessToken: string;
+    UserData: UserEntity;
+  }> {
+    const UserData = await this.usersService.signin(userSignin);
+    const accessToken = await this.usersService.getAccessToken(UserData);
+
+    return { accessToken, UserData };
   }
 
   @Post()
